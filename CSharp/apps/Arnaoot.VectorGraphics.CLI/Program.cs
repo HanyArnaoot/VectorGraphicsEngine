f8813f542d2f56a47082ef76b6fd8e297688b609
Hany Arnaoot <hanyarnaoot@yahoo.com>
2025-12-07 18:08:44 +0200

    Add files via upload

---
+++ CSharp/apps/Arnaoot.VectorGraphics.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿ namespace Arnaoot.VectorGraphics.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         }
+       
+    }
+
+}
---
+++ CSharp/apps/LinuxVectorDrawEngine/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LinuxVectorDrawEngine
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
